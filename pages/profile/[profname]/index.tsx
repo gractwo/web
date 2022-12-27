362c829c135598cbd2cd2e9332f83b16b8c8d615
jakubmanczak <jakub@manczak.net>
2022-12-27 14:22:15 +0100

    make userprofiles rely on fetched data rather than local json

--- pages/profile/[profname]/index.tsx
+++ pages/profile/[profname]/index.tsx
@@ -1,71 +1,103 @@
 import { useRouter } from "next/router";
-import administracja from "../../../data/administracja.json";
+import { useEffect, useState } from "react";
+import { SEO } from "../../../components/SEO";
 
 const ProfilePage = () => {
 	const router = useRouter();
 	const profname = router.query.profname as string;
-	if (
-		administracja
-			.map((el) => {
-				return el.name.replaceAll(" ", "-").toLocaleLowerCase();
+	type personsSchema = {
+		Id: string;
+		Name: string;
+		Desc?: string;
+		Img?: string;
+		IsAdmin?: boolean;
+		DevBadge?: boolean;
+		AssignedUser?: string;
+	};
+	const [persons, setPersons] = useState([]);
+	const [loading, setLoading] = useState(true);
+	useEffect(() => {
+		fetch("https://gractwo.pl/api/v1/admincards")
+			.then((res) => {
+				return res.json();
 			})
-			.includes(profname)
-	) {
-		const person = administracja.filter((wpis) => {
-			return wpis.name.replaceAll(" ", "-").toLocaleLowerCase() === profname;
-		})[0];
-		return (
-			<main>
-				<div
-					style={{
-						display: "flex",
-						flexDirection: "row",
-						alignItems: "center",
-						gap: "1rem",
-						marginBottom: "1rem",
-					}}
-				>
-					<img
-						src={person.img}
-						alt={`${person.name} profile image`}
-						style={{
-							width: "128px",
-							aspectRatio: "1/1",
-							objectFit: "cover",
-							borderRadius: "50%",
-						}}
-					/>
-					<div>
-						<h1>{person.name}</h1>
-						<p>{person.desc}</p>
-					</div>
-				</div>
-				{person.profile?.bigdesc.map((el, index) => {
-					return <p key={index}>{el || <br />}</p>;
-				})}
-			</main>
-		);
+			.then((data) => {
+				setPersons(data);
+				setLoading(false);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
+	if (loading) {
+		return <main style={{ color: "grey" }}>Fetching data...</main>;
 	} else {
-		return (
-			<main>
-				<h1>Sorki!{" :("}</h1>
-				<p style={{ lineHeight: "30px" }}>
-					Sprawdź pisownię:
-					<span
+		if (
+			persons
+				.map((el: personsSchema) => {
+					return el.Name.replaceAll(" ", "-").toLocaleLowerCase();
+				})
+				.includes(profname)
+		) {
+			const person: personsSchema = persons.filter((wpis: personsSchema) => {
+				return wpis.Name.replaceAll(" ", "-").toLocaleLowerCase() === profname;
+			})[0];
+			return (
+				<main>
+					<SEO title={person.Name} />
+					<div
 						style={{
-							background: "var(--black1)",
-							boxShadow: "var(--shadow0)",
-							padding: "8px",
-							margin: "10px",
-							borderRadius: "4px",
+							display: "flex",
+							flexDirection: "row",
+							alignItems: "center",
+							gap: "1rem",
+							marginBottom: "1rem",
 						}}
 					>
-						{`/profile/${profname}`}
-					</span>
-					nie jest poprawnym lokatorem profilu.
-				</p>
-			</main>
-		);
+						<img
+							src={person.Img}
+							alt={`${person.Name} profile image`}
+							style={{
+								width: "128px",
+								aspectRatio: "1/1",
+								objectFit: "cover",
+								borderRadius: "50%",
+							}}
+						/>
+						<div>
+							<h1>{person.Name}</h1>
+							<p>{person.Desc}</p>
+						</div>
+					</div>
+					{/* FOR LATER BIGDESC DATASET */}
+					{/* {person.profile?.bigdesc.map((el, index) => {
+						return <p key={index}>{el || <br />}</p>;
+					})} */}
+				</main>
+			);
+		} else {
+			return (
+				<main>
+					<SEO title="nieznany profil" />
+					<h1>Sorki!{" :("}</h1>
+					<p style={{ lineHeight: "30px" }}>
+						Sprawdź pisownię:
+						<span
+							style={{
+								background: "var(--black1)",
+								boxShadow: "var(--shadow0)",
+								padding: "8px",
+								margin: "10px",
+								borderRadius: "4px",
+							}}
+						>
+							{`/profile/${profname}`}
+						</span>
+						nie jest poprawnym lokatorem profilu.
+					</p>
+				</main>
+			);
+		}
 	}
 };
 
